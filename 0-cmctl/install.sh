4cbef72e60e527ab5441daf005ee3ff740d1a288
Alexander van Trijffel <alexander@structura.ws>
2023-02-06 16:27:14 +0000

    feat: cmctl install

---
+++ 0-cmctl/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e 
+
+OS=$(go env GOOS); ARCH=$(go env GOARCH); curl -fsSL -o cmctl.tar.gz https://github.com/cert-manager/cert-manager/releases/latest/download/cmctl-$OS-$ARCH.tar.gz
+tar xzf cmctl.tar.gz
+sudo mv cmctl /usr/local/bin
